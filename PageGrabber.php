ea4669db03a11c3e463284092936e0616c232024
allimist <allimist@gmail.com>
2016-04-29 13:03:57 +0300

    Update PageGrabber.php

--- PageGrabber.php
+++ PageGrabber.php
@@ -18,7 +18,7 @@ class PageGrabber {
      * Get the title from the url
      * @return string the title of the URL page
      */
-    public function get_title() {
+    public function getTitle() {
 	@$content = file_get_contents($this->url, FILE_USE_INCLUDE_PATH); //ignore warnings
 	if(strlen($content) > 0) {
             preg_match("/<title.*>(.*)<\/title>/i", $content, $title); // ignore case sensitive
